8aa19a78df22358a2d574fcf146f237d7c4dc116
Jon Ish <jon_ish@yahoo.com>
2021-01-27 18:35:14 +0000

    practice git

---
+++ week2/HelloWorld.py
@@ -0,0 +1,5 @@
+#Hello World
+#Prints, "Hello World"
+#Authour: Jon Ishaque
+print ("Hello World\nHow are you today?")
+
---
+++ week2/readname.py
@@ -0,0 +1,8 @@
+#readinname and print it out
+#Author: Jon Ishaque
+
+name = input('Enter your name: ')
+print('Hello '+ name)
+
+age = input('What is your age: ')
+print('Your age is {}'.format(age))

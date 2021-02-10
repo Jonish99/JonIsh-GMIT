4db6eb7737018d24f92c357159c206acd795857d
Jon Ish <jon_ish@yahoo.com>
2021-02-10 20:56:07 +0000

    (Wk2 renamed)

---
+++ Week02/AddOne.py
@@ -0,0 +1,8 @@
+#read a variable and add one to it
+#Author: Jon Ishaque
+
+
+#enter a number, convert input to an int
+Num=int(input('Please enter a number:'))
+NewNum = Num + 1 
+print('{} + 1 is {}'.format(Num,NewNum))
---
+++ Week02/HelloName.py
@@ -0,0 +1,7 @@
+#Uses a variable to greet 
+#author: Jon Ishaque
+name="Jon"
+print("Hello," + name)
+age=45
+print('Your age is: '+str(age))
+print('Hello {}\tYour age is {}'.format(name,age))

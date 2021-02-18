a0c8e9443b7d360d913ae6601b274403994844f6
Jonish99 <77638521+Jonish99@users.noreply.github.com>
2021-02-18 10:12:07 +0000

    Delete Week02 directory
    Deleted old directory

--- Week02/AddOne.py
+++
@@ -1,8 +0,0 @@
-#read a variable and add one to it
-#Author: Jon Ishaque
-
-
-#enter a number, convert input to an int
-Num=int(input('Please enter a number:'))
-NewNum = Num + 1 
-print('{} + 1 is {}'.format(Num,NewNum))

c01a7b39b3ff653fe981e2aa50e833798e9d315e
Jonish99 <77638521+Jonish99@users.noreply.github.com>
2021-02-24 22:49:07 +0000

    Delete Week 02 directory

--- Week 02/HelloName.py
+++
@@ -1,7 +0,0 @@
-#Uses a variable to greet 
-#author: Jon Ishaque
-name="Jon"
-print("Hello," + name)
-age=45
-print('Your age is: '+str(age))
-print('Hello {}\tYour age is {}'.format(name,age))
--- Week 02/ReadName.py
+++
@@ -1,9 +0,0 @@
-#readinname and print it out
-#Author: Jon Ishaque
-
-#input msg, assign input to var name and print msg
-name = input('Enter your name: ')
-print('Hello '+ name)
-#input msg, assign input to var name and print msg
-age = input('What is your age: ')
-print('Your age is {}'.format(age))

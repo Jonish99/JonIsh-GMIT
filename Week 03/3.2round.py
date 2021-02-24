7ad6751eb2fe9ea71dd5f55df607d6324fc0c536
Jonish99 <77638521+Jonish99@users.noreply.github.com>
2021-02-24 22:49:23 +0000

    Delete Week 03 directory

--- Week 03/3.2round.py
+++
@@ -1,8 +0,0 @@
-#a  programme to take a float and output an int
-#Author:Jon Ishaque
-
-numToRound=float(input("Enter a float number:"))
-roundNum=round(numToRound)
-
-#output and format
-print("{} rounded is, {}".format(numToRound,roundNum))

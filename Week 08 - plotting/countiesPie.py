ff46111142aa018ff45f29fa56371d8fa8f69cf6
Jon Ish <jon_ish@yahoo.com>
2021-03-11 18:38:34 +0000

    added counties bar and pie chart

---
+++ Week 08 - plotting/countiesPie.py
@@ -0,0 +1,22 @@
+#A program that makes a pie plot o fthe unique occurences value sin an array
+#Author: Jon Ishaque
+
+import numpy as np 
+import matplotlib.pyplot as plt
+
+#create an array
+possibleCounties =['Cork','Kerry','Waterford','Limerick','Tipperary']
+
+#pick 100 randomly from possible countries with the frequency set in p
+
+counties = np.random.choice(possibleCounties, p=[0.1, 0.3, 0.2, 0.12, 0.28], size=(100))
+
+#get the number of occurences of each county, returns a tuple of the uniqque values and how many times they appear
+#research this
+unique, counts = np.unique(counties,return_counts=True)
+
+#add to a pie plot
+
+plt.pie(counts,labels=unique)
+
+plt.show()
